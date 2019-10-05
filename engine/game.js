fa61776005d255193b2ddb1c3a2aeca5ea7d3c3d
Demolishun <me@demolishun.com>
2019-10-05 10:30:50 -0600

    Changes from hackathon

--- engine/game.js
+++ engine/game.js
@@ -2,18 +2,52 @@ class Game {
   constructor(config) {
     this.showHitboxes = false
 
+    this.entities = []
+
     const element = config.stage
     this.width = element.width
     this.height = element.height
     this.stage = element.getContext('2d')
 
-    // Temporary Hello World
-    this.stage.fillStyle = '#ecf0f1'
-    this.stage.textAlign = 'center'
-    this.stage.font = '24px sans-serif'
-    this.stage.fillText('Stage Set And Ready To Work!', this.width / 2, this.height / 2)
-    this.stage.fillStyle = '#95a5a6'
-    this.stage.font = '16px sans-serif'
-    this.stage.fillText('If everything loaded correctly, we can get started.', this.width / 2, this.height / 2 + 32)
+    this.player = Characters.Player(32,32)
+    this.entities.push(this.player)
+
+    //console.dir()
+    this.input = new Input(this.player, this)
+
+    assets.download(() => this.loop())
+  }
+
+  loop() {
+    this.entities.forEach(entity => {
+      // todo: movement, animation, ...
+      if (entity.has(['active','position','velocity'])) this.move(entity)
+    })
+
+    this.render()
+
+    // suspends call if busy
+    window.requestAnimationFrame(() => this.loop())
+  }
+
+  move(entity) {
+    if (!entity.com('velocity').vx && !entity.com('velocity').vy) return
+    // todo: collision detection
+
+    entity.com('position').x += entity.com('velocity').vx
+    entity.com('position').y += entity.com('velocity').vy
+  }
+
+  render() {
+    const updates = []
+
+    this.entities.forEach(entity  => {
+      if(entity.has(['active', 'renderer'])) updates.push(entity.com('renderer'))
+    })
+
+    if (!updates.length) return
+
+    this.stage.clearRect(0, 0, this.width, this.height)
+    updates.forEach(update => update.run(this.stage))
   }
 }
